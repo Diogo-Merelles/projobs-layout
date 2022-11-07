8be1fcf1666db5e26516231ea6774e949ea54ee6
Diogo Merelles <diogo.merelles.faria@gmail.com>
2022-11-07 16:03:36 +0000

    added show  and hide button with rotating icon

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const targetDiv = document.getElementById("hidden-tab");
 const btn = document.getElementById("toggle-btn");
+const arrow = document.getElementById("arrow-up");
 
-btn.onclick = function () {
+btn.onclick = () => {
   if (targetDiv.style.display !== "block") {
     targetDiv.style.display = "block";
+    arrow.style.transform = "rotate(45deg)";
+
   } else {
     targetDiv.style.display = "none";
+    arrow.style.transform = "rotate(-135deg)";
+    // targetDiv.style.transition = "5s ease-in-out";
+
   }
 };
