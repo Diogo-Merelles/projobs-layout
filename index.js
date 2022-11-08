135b578affb9b2afc6a626993368982bc194a1ca
Diogo Merelles <diogo.merelles.faria@gmail.com>
2022-11-08 15:59:59 +0000

    added close button fo hidden tab, highligth buttons onclick, general layout

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const targetDiv = document.getElementById("hidden-tab");
 const btn = document.getElementById("toggle-btn");
 const arrow = document.getElementById("arrow-up");
+const carouselBtn = document.querySelectorAll(".top-carousel-button");
+const closeHiddenTab = document.getElementById("fa fa-close");
 
-btn.onclick = () => {
-  if (targetDiv.style.display !== "block") {
-    targetDiv.style.display = "block";
-    arrow.style.transform = "rotate(45deg)";
-
-  } else {
+closeHiddenTab.onclick = () => {
+  if (closeHiddenTab) {
     targetDiv.style.display = "none";
-    arrow.style.transform = "rotate(-135deg)";
-    // targetDiv.style.transition = "5s ease-in-out";
-
+    arrow.style.transform = "rotate(45deg)";
   }
 };
+
+btn.onclick = () => {
+  if (targetDiv.style.display !== "flex") {
+    targetDiv.style.display = "flex";
+    arrow.style.transform = "rotate(-135deg)";
+  //   arrow.animate([
+  //   `@Keyframes arrow-up {
+  //     from {
+  //       rotate(-135deg);
+  //     }
+  //     to {
+  //       rotate(45deg)
+  //     }
+  //   }`
+  // ], {
+  //   duration: 2000
+  // });
+  } else {
+    targetDiv.style.display = "none";
+    arrow.style.transform = "rotate(45deg)";
+    // targetDiv.style.transition = "5s ease-in-out";
+  }
+};
+
+// Cant leave the button highlighted on reload
+for (button in carouselBtn) {
+  carouselBtn[button].onclick = function () {
+    //console.log("test");
+    let highligthBtn = document.querySelectorAll(".highligth-carousel")[0];
+    if (this.className == "top-carousel-button") {
+      if (highligthBtn) highligthBtn.className = "top-carousel-button";
+      this.className = "highligth-carousel";
+    }
+  };
+}
